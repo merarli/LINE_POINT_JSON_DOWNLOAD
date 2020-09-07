0b7609492abac4f7973636ef9e065937329ab21c
merarli <kenji@mera.tokyo>
2020-09-07 16:01:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# はじめに
+LINEポイントの獲得、使用履歴をJSONでダウンロードできます。
+
+# 使い方
+
+## インストール
+このプロジェクトをクローンしてから``src``ディレクトリを
+Google Chromeの拡張機能 > 「パッケージ化されていない拡張機能を読み込む」からインストールしてください
+
+## LINEポイントにアクセス
+``https://points.line.me/point_history?.ref=header#/point_history/deposit``
+にアクセスして、Google Chromeのデベロッパーツールで、デバイスを対応しているiOS端末名を指定してください。
+
+## JSONダウンロード
+保存する範囲を広げるために、ページを下へし続けてください。レンダリングされた範囲が保存されます。
+JSONダウンロードを実行するためには、そのページでキーボードの``e``キーを押してください
+
+# License
+
+MIT
+
+# 引用
+https://github.com/waterada/chrome-ex-template
---
+++ src/content_script.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+  console.log('拡張機能読み込みました')
+})
+
+const exportJson = () => {
+  console.log('exportJson...')
+  const titles = $('.MdCMN25AcquisitionList > li .mdCMN25Sumally > .mdCMN25Price > span')
+  const dates = $('.MdCMN25AcquisitionList > li .mdCMN25Sumally > .mdCMN25Date')
+  const points = $('.MdCMN25AcquisitionList > li .mdCMN25Point > .vss-SkeletonPaddingY1-radius > .mdCMN25PointTxt')
+
+  let point_list = []
+  for (let i = 0; i < titles.length; i++) {
+    point_list.push({
+      title: titles.eq(i).text(),
+      date: dates.eq(i).text(),
+      point: points.eq(i).text()
+    })
+  }
+
+  console.table(point_list)
+  downloadObjectAsJson(point_list,'line_point')
+}
+
+downloadObjectAsJson = (exportObj, exportName) => {
+  const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+  const downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href",     dataStr);
+  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
+$('body').keydown(event => {
+  if (event.key === 'e') {
+    exportJson()
+  }
+})
